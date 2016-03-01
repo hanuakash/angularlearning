9fcf90a52bc237d13916abed42d517d715d31b53
kneerunjun <kneerunjun@outlook.com>
2016-03-01 14:57:32 +0530

    simple double binding experience in angular

---
+++ webapp/ng/controllers/mainController.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    var mainController = angular.module("myApp").controller("mainController", function ($scope) {
+        $scope.user = {
+            title:"Niranjan"
+        }
+    })
+})();
