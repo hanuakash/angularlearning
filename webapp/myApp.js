4f1d33104a433764679f94dd4e8a77d3c005aed4
kneerunjun <kneerunjun@outlook.com>
2016-03-02 10:36:39 +0530

    demo of route provide and url configuration

--- webapp/myApp.js
+++ webapp/myApp.js
@@ -1,3 +1,9 @@
 ﻿(function () {
-    var myApp = angular.module("myApp", [])
+    var myApp = angular.module("myApp", ["ngRoute"]).config(function ($routeProvider) {
+        $routeProvider
+        .when("/", { templateUrl: "/ng/views/login.html", controller: "" })
+        .when("/login", { templateUrl: "/ng/views/login.html", controller: "" })
+        .when("/reportees", { templateUrl: "/ng/views/reportees.html", controller: "" })
+        .otherwise({ redirectTo: "/login" })
+    })
 })();
