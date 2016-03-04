1c5c96577970cd4aeac26fe9c41bf6f92a90589d
kneerunjun <kneerunjun@outlook.com>
2016-03-04 11:38:55 +0530

    demo of transclusion

---
+++ webapp/ng/directives/my-frame.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var myFrame = angular.module("myApp").directive("myFrame", function () {
+        return {
+            restrict: "E",
+            replace: true,
+            scope: {
+
+            },
+            transclude:true,
+            templateUrl:"/ng/templates/my-frame.html",
+            controller: function ($scope) { },
+            link: function (scope, elem, attrs, ctroller) { }
+        }
+    })
+})();
