ff0d1de3e0f707a0adb0eebe3e0629f6b2dcbc85
kneerunjun <kneerunjun@gmail.com>
2016-05-25 10:17:48 +0530

    login validation

--- ng/controllers/loginController.js
+++ ng/controllers/loginController.js
@@ -1,16 +1,28 @@
 ﻿(function () {
     var loginController = angular.module("myApp").controller("loginController", function ($scope) {
        
-
+        var validation = function () {
+            $scope.invalid.username = $scope.user.username.length > 0 ? false : true;
+            $scope.invalid.password = $scope.user.password.length > 0 ? false : true;
+            return !$scope.invalid.username && !$scope.invalid.password;
+        }
+        $scope.invalid = {
+            username: false,
+            password:false
+        };
         //this is yet another way of thinkin
         $scope.user = {
             username: "",
             password:""
         }
         $scope.login = function () {
-            console.debug($scope.user.username);
-            console.debug($scope.user.password);
-            console.log("we are ready to login the user");
+            if (validation() == true) {
+                console.log("validation is complete");
+                console.log("we are ready to make a http calls");
+            }
+            else {
+                console.error("the validation has failed");
+            }
         }
        
     })
