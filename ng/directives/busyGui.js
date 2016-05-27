2bb6064da9905691298270f53cb9b4980ccb9d2b
kneerunjun <kneerunjun@gmail.com>
2016-05-27 12:16:59 +0530

    demo of transcluding directives

--- ng/directives/busyGui.js
+++ ng/directives/busyGui.js
@@ -1,10 +1,12 @@
 ﻿(function () {
     var busyGui = angular.module("myApp").directive("busyGui", function () {
         return {
-            restrict: "E",
+            restrict: "A",
             replace: true,
             scope: {},
             controller: function ($scope, $rootScope) {
+
+                console.log("we are inside the controller");
                 $scope.animation = false;
 
                 $rootScope.$on("busy", function (evt, data) {
@@ -17,7 +19,18 @@
             },
             templateUrl: "/ng/templates/busyGui.html",
             transclude: true,
-
+           
+            compile: function (tElem, tAttr) {
+                console.log("we are inside the compile function");
+                return {
+                    pre: function (scope, ele, attrs) {
+                        console.log("we are inside the pre function");
+                    },
+                    post: function (scope, ele, attrs) {
+                        console.log("we are inside the post function");
+                    }
+                }
+            }
         }
     })
 })()
