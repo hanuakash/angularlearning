9cf0b6a3929717e3c25c12591718e67c25b5a25e
kneerunjun <kneerunjun@gmail.com>
2016-06-07 14:39:30 +0530

    first encounter with controller and app

---
+++ ng/myApp.js
@@ -0,0 +1,8 @@
+﻿//dependent modules go in that array, else empty
+
+(function () {
+    var myApp = angular.module("myApp", [])
+    var mainController = myApp.controller("mainController", function ($scope) {
+        $scope.message = "Hi there , from inside the controller";
+    })
+})() //IIFE - immediately invoked function expression
