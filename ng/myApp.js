fe3a8e11766107fc720b7ff44e2d5b3404cab737
kneerunjun <kneerunjun@gmail.com>
2016-06-07 15:00:06 +0530

    first encounter with controller and app

--- ng/myApp.js
+++ ng/myApp.js
@@ -4,5 +4,11 @@
     var myApp = angular.module("myApp", [])
     var mainController = myApp.controller("mainController", function ($scope) {
         $scope.message = "Hi there , from inside the controller";
+        $scope.someMessage = "this is some message from inside the controller";
+
+    })
+    var innerController = myApp.controller("innerController", function ($scope) {
+        $scope.syriaMessage = "Raqqa is ours, ISIL has been repelled!";
+        $scope.message = "this is from the inner controller";
     })
 })() //IIFE - immediately invoked function expression
