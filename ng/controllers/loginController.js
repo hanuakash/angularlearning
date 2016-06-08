295007e7a5a6d2515d2cbfbee7968d1cd3990e9e
kneerunjun <kneerunjun@gmail.com>
2016-06-08 15:28:29 +0530

    http as a injectable service

--- ng/controllers/loginController.js
+++ ng/controllers/loginController.js
@@ -1,13 +1,38 @@
 ﻿(function () {
-    var loginController = angular.module("myApp").controller("loginController", function ($scope) {
+    var loginController = angular.module("myApp").controller("loginController", function ($scope, $http) {
+        $scope.showWarning = false;
         $scope.userInfo = {
             username: "",
             password:""
         };
+        var validated = function () {
+            return $scope.userInfo.username !== "" && $scope.userInfo.password !== "" ? true : false;
+        }
         $scope.login = function () {
-            console.log("ARIZONA!!!");
-            console.log($scope.userInfo.username);
-            console.log($scope.userInfo.password);
+            if (validated() == true) {
+                $scope.showWarning = false;
+                console.log("We are ready to login and hit the webservice");
+                //this is where you go and hit the service
+                //VERBS:
+                //GET
+                //PUT
+                //PATCH
+                //POST
+                //DEL
+                $http.get("http://localhost:8080/myapi/users/username").then(function (response) {
+                    console.log("success response");
+                    console.log(response.data);
+                    console.log(response.status);
+                }, function (response) {
+                    console.log("error response");
+                    console.log(response.ExceptionMessage);
+                    console.log(response.status);
+                })
+            }
+            else {
+                $scope.showWarning = true;
+                console.log("Better check the username and password");
+            }
         }
     })
 })();
