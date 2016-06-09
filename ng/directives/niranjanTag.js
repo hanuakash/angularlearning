ea077e4ba04bcdfee64ec5eb5206be1e03dea062
kneerunjun <kneerunjun@gmail.com>
2016-06-09 15:36:51 +0530

    first directive

---
+++ ng/directives/niranjanTag.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var niranjanTag = angular.module("myApp").directive("niranjanTag", function () {
+        return {
+            restrict: "E",
+            replace: false,
+            scope: {},
+            templateUrl:"ng/templates/niranjanTag.html"
+        }
+    })
+})();
