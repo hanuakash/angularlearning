f2230642e2dc1bb98f0fd84730bd9775c4b5e310
kneerunjun <kneerunjun@gmail.com>
2016-06-09 14:28:20 +0530

    demo for ng-repeat

---
+++ ng/controllers/employeesController.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    var employeesController = angular.module("myApp").controller("employeesController", function ($scope) {
+        $scope.employees = [
+            { empno: 41993, email: "niranjan_awati", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41992, email: "vsdgfdgdg", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41994, email: "niranjan_agwati", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41995, email: "gfdgfdg", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41996, email: "gdfgdf", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41997, email: "dfgfdg", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41998, email: "fgdgdf", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41950, email: "gdfg", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41967, email: "gfdgd", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41987, email: "gfdgdfsdfdsfd", alias: "Niranjan", location: "Pune", du: "ETA" },
+        ];
+
+        $scope.popEmployee = function () {
+            $scope.employees.splice(0, 1);
+        }
+    })
+})();
--- ng/myApp.js
+++ ng/myApp.js
@@ -7,6 +7,7 @@
         .when("/", { templateUrl: "/ng/views/login.html", controller: "loginController" })
         .when("/login", { templateUrl: "/ng/views/login.html", controller: "loginController" })
         .when("/settings", { templateUrl: "/ng/views/settings.html", controller: "" })
+        .when("/employees", { templateUrl: "/ng/views/employees.html", controller: "employeesController" })
         .otherwise({redirectTo:"/"})
     })
 })() //IIFE - immediately invoked function expression
