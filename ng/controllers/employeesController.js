2239b4a3af00b3b4d415573bda74e435270dce70
kneerunjun <kneerunjun@gmail.com>
2016-06-09 14:53:48 +0530

    selection of tabulr entities, ng-class

--- ng/controllers/employeesController.js
+++ ng/controllers/employeesController.js
@@ -1,20 +1,29 @@
 ﻿(function () {
     var employeesController = angular.module("myApp").controller("employeesController", function ($scope) {
+        
         $scope.employees = [
-            { empno: 41993, email: "niranjan_awati", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41992, email: "vsdgfdgdg", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41994, email: "niranjan_agwati", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41995, email: "gfdgfdg", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41996, email: "gdfgdf", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41997, email: "dfgfdg", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41998, email: "fgdgdf", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41950, email: "gdfg", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41967, email: "gfdgd", alias: "Niranjan", location: "Pune", du: "ETA" },
-            { empno: 41987, email: "gfdgdfsdfdsfd", alias: "Niranjan", location: "Pune", du: "ETA" },
+            { empno: 41993, email: "niranjan_awati", alias: "Niranjan", location: "Pune", du: "ETA", sel:false },
+            { empno: 41992, email: "vsdgfdgdg", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41994, email: "niranjan_agwati", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41995, email: "gfdgfdg", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41996, email: "gdfgdf", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41997, email: "dfgfdg", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41998, email: "fgdgdf", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41950, email: "gdfg", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41967, email: "gfdgd", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
+            { empno: 41987, email: "gfdgdfsdfdsfd", alias: "Niranjan", location: "Pune", du: "ETA", sel: false },
         ];
 
-        $scope.popEmployee = function () {
-            $scope.employees.splice(0, 1);
+        $scope.saveChanges = function () {
+            //here we are trying to get all the emps that are selected
+            var selected = $scope.employees.filter(function (el, index) {
+                return el.sel == true;
+            });
+            console.table(selected);
+        }
+        
+        $scope.toggleSelectRow = function (e) {
+            e.sel = !e.sel;
         }
     })
 })();
