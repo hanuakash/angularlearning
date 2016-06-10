0f35fbd72861ad7417d5f24bfa146f38444bf2a5
kneerunjun <kneerunjun@gmail.com>
2016-06-10 16:01:56 +0530

    inter directive communication

--- ng/controllers/settingsController.js
+++ ng/controllers/settingsController.js
@@ -1,9 +1,12 @@
 ﻿(function () {
-    var settingsController = angular.module("myApp").controller("settingsController", function ($scope) {
+    var settingsController = angular.module("myApp").controller("settingsController", function ($scope, $rootScope) {
         $scope.user = {
             alias: "niranjan",
             location: "Pune",
             empno:41993
+        }
+        $scope.sendEvent = function () {
+            $rootScope.$broadcast("ping", { message: "this is hi from inside the controller" });
         }
     })
 })();
--- ng/directives/levelOne.js
+++ ng/directives/levelOne.js
@@ -2,11 +2,48 @@
     var levelOne = angular.module("myApp").directive("levelOne", function () {
         return {
             restrict: "E",
-            scope: true, //inherited scope
-            controller: function ($scope) {
-                $scope.user = { alias: "niranjan awati" };
+            scope: {},
+            controller: function ($scope, $rootScope) {
+                $scope.message = "";
+                $rootScope.$on("ping", function (evt, data) {
+                    $scope.message = data.message;
+                });
+                
             },
-            templateUrl:"ng/templates/levelOne.html"
+            templateUrl: "ng/templates/levelOne.html",
+            transclude: true,
+            compile: function (telem, tattrs) {
+                console.log("level-one:we are inside the compile function");
+                return {
+                    pre: function (scope, elem,attrs) {
+                        console.log("level-one:we are inside the pre function");
+                    },
+                    post: function (scope,elem, attrs) {
+                        console.log("level-one:we are inside the post function");
+                    }
+                }
+            },
         }
-    })
+    });
+    var levelTwo = angular.module("myApp").directive("levelTwo", function () {
+        return {
+            restrict: "E",
+            scope: {},
+            template:"<div></div>",
+            controller: function ($scope) {
+                console.log("level-two:we are inside the controller");
+            },
+            compile: function (telem, tattrs) {
+                console.log("level-two:we are inside the compile function");
+                return {
+                    pre: function (scope, elem, attrs) {
+                        console.log("level-two:we are inside the pre function");
+                    },
+                    post: function (scope, elem, attrs) {
+                        console.log("level-two:we are inside the post function");
+                    }
+                }
+            },
+        }
+    });
 })();
