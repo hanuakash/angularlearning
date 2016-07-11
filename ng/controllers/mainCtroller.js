525c5462ddf84ff3988e18291b8ddd71513034be
kneerunjun <kneerunjun@gmail.com>
2016-07-11 12:02:32 +0530

    adding new ly created project

---
+++ ng/controllers/mainCtroller.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var mainCtroller = angular.module("myApp").controller("mainCtroller", function ($scope) {
+        $scope.message = "Hi, this is from inside the controller";
+        $scope.user = {
+            email: "niranjan_awati",
+            empno: 41993
+        };
+        $scope.locations = [];
+    })
+})();
---
+++ ng/myApp.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module("myApp", [])
+})(); //IIFE - immediately invoked fucntion expression
