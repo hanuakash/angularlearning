fbeb4c9448b2ae3b66128913e6396f6746bd672c
kneerunjun <kneerunjun@gmail.com>
2016-07-12 10:32:28 +0530

    this is for the simple demo on routeprovider

---
+++ ng/controllers/loginCtroller.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    angular.module("myApp").controller("loginCtroller", function ($scope) {
+        $scope.message = "This is Hi from inside the controller"
+        $scope.user = {
+            alias: "niranjan",
+            empno:41993
+        }
+    })
+})();
--- ng/myApp.js
+++ ng/myApp.js
@@ -1,3 +1,11 @@
 ﻿(function () {
-    angular.module("myApp", [])
+    angular.module("myApp", ["ngRoute"])
+    .config(function ($routeProvider) {
+        $routeProvider
+        .when("/", { templateUrl: "ng/views/login.html", controller: "loginCtroller" })
+        .when("/login", { templateUrl: "ng/views/login.html", controller: "loginCtroller" })
+        .when("/settings", { templateUrl: "ng/views/settings.html", controller: "" })
+        .when("/employees", { templateUrl: "ng/views/employees.html", controller: "" })
+        .otherwise({redirectTo:"/"})
+    })
 })(); //IIFE - immediately invoked fucntion expression
