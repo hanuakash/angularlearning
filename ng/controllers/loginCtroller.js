0a71b519bbd3642ed502a11f5e74a9586f13afe5
kneerunjun <kneerunjun@gmail.com>
2016-07-12 11:31:39 +0530

    login with validation

--- ng/controllers/loginCtroller.js
+++ ng/controllers/loginCtroller.js
@@ -1,12 +1,43 @@
 ﻿(function () {
-    angular.module("myApp").controller("loginCtroller", function ($scope, $location) {
-        $scope.message = "This is Hi from inside the controller"
+    angular.module("myApp").controller("loginCtroller", function ($scope, $route) {
+        $scope.warning = null;
         $scope.user = {
-            alias: "niranjan",
-            empno: 41993
+            email: "",
+            password: ""
         };
-        $scope.clicked = function () {
-            $location.url("/settings")
+        var validation = function () {
+            if ($scope.user.email !== "" && /@/.test($scope.user.email) == false) {
+                if ($scope.user.password !== "") {
+                    $scope.warning = null;
+                    return true;
+                }
+                else {
+                    $scope.warning = {
+                        message: "There is something wrong with the password, better check yourself",
+                        errorEmail: false,
+                        errorPassword: true
+
+                    }
+                }
+            }
+            else {
+                $scope.warning = {
+                    message: "There is something wrong with the email, better check yourself",
+                    errorEmail: true,
+                    errorPassword: false
+                }
+            }
+        }
+        $scope.login = function () {
+            if (validation() == true) {
+                console.log("we are ready to make the http call");
+            }
+            else {
+                console.error("User input incorrect")
+            }
+        }
+        $scope.retry = function () {
+            $route.reload();
         }
     })
 })();
