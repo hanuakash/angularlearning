9e79dd078d58cb545d725d8374a5611541a6cbeb
kneerunjun <kneerunjun@gmail.com>
2016-07-12 10:37:49 +0530

    moving urls via code

--- ng/controllers/loginCtroller.js
+++ ng/controllers/loginCtroller.js
@@ -1,9 +1,12 @@
 ﻿(function () {
-    angular.module("myApp").controller("loginCtroller", function ($scope) {
+    angular.module("myApp").controller("loginCtroller", function ($scope, $location) {
         $scope.message = "This is Hi from inside the controller"
         $scope.user = {
             alias: "niranjan",
-            empno:41993
+            empno: 41993
+        };
+        $scope.clicked = function () {
+            $location.url("/settings")
         }
     })
 })();
